5dbeb93ea9296ef601942c6404f09feb807889ba
gbenga504 <daveanifowoshe@ymail.com>
2018-01-12 13:50:47 +0100

    Add reducer and getVersion function to  CoposerClient prototypal methods

--- ComposerClient.js
+++ ComposerClient.js
@@ -1,14 +1,19 @@
 import { version } from "./version";
 import createComposerThunk from "./middlewares/composerThunk";
+import * as composerReducers from "./reducers";
 
 export default function ComposerClient({ endpoint } = { endpoint: undefined }) {
   this.version = version;
   this.endpoint = endpoint;
 }
 
-ComposerClient.prototype.getVersion = function() {};
+ComposerClient.prototype.getVersion = function() {
+  return this.version;
+};
 
-ComposerClient.prototype.reducer = function() {};
+ComposerClient.prototype.reducer = function() {
+  return Object.assign({}, composerReducers);
+};
 
 ComposerClient.prototype.middleware = function({
   useBeforeRequest,
