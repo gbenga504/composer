1faa48e8fc86ee1bb6430fc2573b30cd5c51c241
gbenga504 <daveanifowoshe@ymail.com>
2018-01-12 12:03:07 +0100

    Add middleware to composeClient

--- ComposerClient.js
+++ ComposerClient.js
@@ -1,4 +1,5 @@
-import { version } from "./version";=
+import { version } from "./version";
+import createComposerThunk from "./middlewares/composerThunk";
 
 export default function ComposerClient({ endpoint } = { endpoint: undefined }) {
   this.version = version;
@@ -9,4 +10,9 @@ ComposerClient.prototype.getVersion = function() {};
 
 ComposerClient.prototype.reducer = function() {};
 
-ComposerClient.prototype.middleware = function() {};
+ComposerClient.prototype.middleware = function({
+  useBeforeRequest,
+  useAfterResponse
+}) {
+  return createComposerThunk({ useBeforeRequest, useAfterResponse });
+};
