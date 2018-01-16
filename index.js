466738c75de664606d9255cfdf05b195db9da3da
gbenga504 <daveanifowoshe@ymail.com>
2018-01-16 16:34:40 +0100

    Add imports and exports to index Js composer file

---
+++ index.js
@@ -0,0 +1,6 @@
+import ComposerClient from "./ComposerClient";
+import ComposerProvider from "./components/ComposerProvider";
+import composer from "./components/composer";
+
+export default ComposerClient;
+export { ComposerProvider, composer };
