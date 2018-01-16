bde57e32f02364a203054a01b34d4c4b0bb2aafa
gbenga504 <daveanifowoshe@ymail.com>
2018-01-16 16:39:56 +0100

    Add helpers and throwable utils to the application

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+export const Helpers = {
+  sanitizeProps: (prevProps, arrayOfExclusion) => {
+    let newProps = Object.keys(prevProps).reduce((acc, value) => {
+      if (!acc[value] && arrayOfExclusion.indexOf(value) === -1) {
+        acc[value] = prevProps[value];
+      }
+      return acc;
+    }, {});
+    return newProps;
+  }
+};
